356038adf51827cf98ef29b485d72e4a4aa6a775
DaraniShri <daranirameshbabu@gmail.com>
2023-09-28 19:33:07 +0530

    adjust(laravel):adjust in data operations

--- laravel-app/resources/views/employees/view.blade.php
+++ laravel-app/resources/views/employees/view.blade.php
@@ -1,23 +1,57 @@
-<div class="table-responsive">
-    <table class="table table-striped table-hover table-condensed">
-        <thead>
-            <tr>
-                <th><strong>Employee ID</strong></th>
-                <th><strong>Employee Name</strong></th>
-                <th><strong>Email</strong></th>
-                <th><strong>Actions</strong></th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($employees as $key => $employee)
-                <tr>    
-                    <td>{{$employee->id}}</td>
-                    <td>{{$employee->employee_name}}</td>
-                    <td>{{$employee->employee_email}}</td>  
-                    <td><a href="delete/{{$employee->id}}">Delete</a></td>
-                    <td><a href="edit/{{$employee->id}}">edit</a></td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+<html>
+    <head>
+    @include('includes.header')
+    </head>
+    <body>
+        <div class="table-responsive">
+            <table id="table">
+                <thead>
+                    <tr>
+                        <th><strong>Employee ID</strong></th>
+                        <th><strong>Employee Name</strong></th>
+                        <th><strong>Email</strong></th>
+                        <th><strong>Actions</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- <script>
+                        $(document).ready( function () {
+                            $('table').DataTable( function({
+                                
+                            }));
+                        } );
+                    </script> -->
+                    @foreach($employees as $key => $employee)
+                    <tr class="employee{{$employee->id}}">
+                        <td>{{$employee->id}}</td>
+                        <td>{{$employee->employee_name}}</td>
+                        <td>{{$employee->employee_email}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        @include('includes.footer')
+    </body>
+</html>
+<script type="text/javascript">
+$(document).ready( function () {
+    var data = [
+        [
+            "1",
+            "System Architect",
+            "Edinburgh@gmail.com",
+            
+        ],
+        [
+            "2",
+            "Director",
+            "Edinburgh@gmail.com",
+            
+        ]
+    ];
+    jquery('#table').DataTable({
+        data: data,
+    });
+});
+</script>
