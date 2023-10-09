15955ef132c7c770db1d19a8f11321544c278812
DaraniShri <daranirameshbabu@gmail.com>
2023-10-09 19:10:23 +0530

    adjust(laravel) : adjust in CMS page, UUID, Authentication

---
+++ laravel-app/app/Models/student.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\UUID;
+
+
+class student extends Model
+{
+    use CrudTrait;
+    use HasFactory;
+    use UUID;
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+}
